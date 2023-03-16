537203b67995821c99132ffff482842d0bf928c5
João Salinas <joao.salinas@outlook.com>
2023-03-16 20:08:17 -0300

    first commit

---
+++ src/api/computador/controllers/computador.ts
@@ -0,0 +1,7 @@
+/**
+ * computador controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::computador.computador');
---
+++ src/api/computador/services/computador.ts
@@ -0,0 +1,7 @@
+/**
+ * computador service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::computador.computador');
